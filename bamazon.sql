43f99bb68342ee32fb1d8f4edd8fcc70f7b37216
jenenneihart <neihart.jenen@gmail.com>
2020-04-22 23:06:02 -0700

    still trying sql

---
+++ bamazon.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE bamazonDB;
+
+use bamazonDB;
+
+CREATE TABLE Products(
+product_name VARCHAR (50),
+price DECIMAL(10,2),
+stockQuantity INTEGER (10) NULL,
--- customer.js
+++ customer.js
@@ -20,7 +20,6 @@ function productItems() {
     })
 
 }
-
 function productID() {
 
     inquirer.prompt([{
@@ -40,13 +39,36 @@ function productID() {
         var userQuant = answer.quant
         console.log("Selected Product ", userQuant, "\n")
         
-    connection.query("SELECT * from products ", [{
+    connection.query("SELECT * FROM products ", [{
             item_id: answer.id
         }], function (err, res) {
             if (err) throw err
             console.table(res)
             var currentStock = res[0].stockQuantity
-            console.log("Current Profuct in Stock: ", currentStock)
+            console.log("Current Product in Stock: ", currentStock)
             var price = res[0].price
             var remainingStock = currentQuantity - answer.quant
             console.log("Remaining Stock: ", remainingStock)
+            
+        }
+    connection.query("UPDATE department_name SET ? WHERE ?", [
+            {TotalSales: deptRes[index].TotalSales + orderTotal},
+            {product_name: res[whatToBuy].product_name}
+        ], function (err, deptRes){
+            if (err) throw err;
+        })
+    })
+} else console.log("There isn't enoughof that item. Try again later.")}
+
+notEnoughPrompt
+         function notEnoughPrompt(){
+                inquirer.prompt([{
+                type: "confirm",
+                name: "reply",
+                message: "Do you want to add anything else?"
+                 }]).then(function(ans){
+                 if(ans.reply){
+                 start();
+                 } else{
+                console.log("See you Again!");
+           
